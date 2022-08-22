79c8645a21bd83e2e72690df1ca72203fd155bfd
anvarxon <akhamzaev@beelab.uz>
2022-08-22 08:40:15 +0500

    Initial commit - console app

---
+++ PublisherConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using PublisherData;
+using PublisherDomain;
+
+using (PubContext context = new PubContext())
+{
+    context.Database.EnsureCreated();
+}
+
+AddAuthors();
+GetAuthors();
+
+void AddAuthors()
+{
+    var author = new Author { FirstName = "Anna", LastName = "Peaks" };
+    using var context = new PubContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+
+void GetAuthors()
+{
+    using var context = new PubContext();
+    var authors = context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}
